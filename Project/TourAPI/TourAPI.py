96c220c3e9794c22736fb7a3f242789d14a8dcc3
YongsunLee <katarite715@naver.com>
2017-05-28 23:45:18 +0900

    UI
    TourAPI.ui 생성 및 py 인코딩

---
+++ Project/TourAPI/TourAPI.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\creat\Desktop\TourAPI.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(837, 539)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(340, 10, 491, 221))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.MapLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.MapLayout.setContentsMargins(0, 0, 0, 0)
+        self.MapLayout.setObjectName("MapLayout")
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 240, 821, 251))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.scrollArea = QtWidgets.QScrollArea(self.verticalLayoutWidget_2)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 817, 247))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.verticalScrollBar = QtWidgets.QScrollBar(self.scrollAreaWidgetContents)
+        self.verticalScrollBar.setGeometry(QtCore.QRect(800, 0, 16, 241))
+        self.verticalScrollBar.setOrientation(QtCore.Qt.Vertical)
+        self.verticalScrollBar.setObjectName("verticalScrollBar")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.addWidget(self.scrollArea)
+        self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(10, 10, 321, 221))
+        self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
+        self.SearchLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
+        self.SearchLayout.setContentsMargins(0, 0, 0, 0)
+        self.SearchLayout.setObjectName("SearchLayout")
+        self.groupBox_2 = QtWidgets.QGroupBox(self.verticalLayoutWidget_3)
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.label_Service = QtWidgets.QLabel(self.groupBox_2)
+        self.label_Service.setGeometry(QtCore.QRect(10, 20, 71, 21))
+        self.label_Service.setObjectName("label_Service")
+        self.comboBox_smallService = QtWidgets.QComboBox(self.groupBox_2)
+        self.comboBox_smallService.setGeometry(QtCore.QRect(80, 60, 231, 21))
+        self.comboBox_smallService.setObjectName("comboBox_smallService")
+        self.label_City = QtWidgets.QLabel(self.groupBox_2)
+        self.label_City.setGeometry(QtCore.QRect(40, 140, 41, 21))
+        self.label_City.setObjectName("label_City")
+        self.comboBox_Area = QtWidgets.QComboBox(self.groupBox_2)
+        self.comboBox_Area.setGeometry(QtCore.QRect(80, 100, 231, 21))
+        self.comboBox_Area.setObjectName("comboBox_Area")
+        self.comboBox_Service = QtWidgets.QComboBox(self.groupBox_2)
+        self.comboBox_Service.setGeometry(QtCore.QRect(80, 20, 151, 21))
+        self.comboBox_Service.setAcceptDrops(False)
+        self.comboBox_Service.setObjectName("comboBox_Service")
+        self.label_Area = QtWidgets.QLabel(self.groupBox_2)
+        self.label_Area.setGeometry(QtCore.QRect(50, 100, 31, 21))
+        self.label_Area.setObjectName("label_Area")
+        self.comboBox_City = QtWidgets.QComboBox(self.groupBox_2)
+        self.comboBox_City.setGeometry(QtCore.QRect(80, 140, 231, 21))
+        self.comboBox_City.setObjectName("comboBox_City")
+        self.label_smallService = QtWidgets.QLabel(self.groupBox_2)
+        self.label_smallService.setGeometry(QtCore.QRect(40, 60, 41, 21))
+        self.label_smallService.setObjectName("label_smallService")
+        self.pushButton = QtWidgets.QPushButton(self.groupBox_2)
+        self.pushButton.setGeometry(QtCore.QRect(210, 172, 101, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.SearchLayout.addWidget(self.groupBox_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 837, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox_2.setTitle(_translate("MainWindow", "Search"))
+        self.label_Service.setText(_translate("MainWindow", "서비스 분류"))
+        self.label_City.setText(_translate("MainWindow", "시군구"))
+        self.label_Area.setText(_translate("MainWindow", "지역"))
+        self.label_smallService.setText(_translate("MainWindow", "소분류"))
+        self.pushButton.setText(_translate("MainWindow", "검색"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
--- Project/TourAPI/main.py
+++ Project/TourAPI/main.py
@@ -1,27 +1,9 @@
 # -*- coding : UTF-8 -*-
-from tkinter import *
-
-class MyFrame(Frame):
-    def __init__(self, master):
-        scrollbar = Scrollbar(master)
-        scrollbar.pack(side=RIGHT,fill=Y)
-        txt = Listbox(master, yscrollcommand=scrollbar.set)
-        txt.insert(1, "서울")
-        txt.insert(2, "인천")
-        txt.insert(3, "인천")
-        txt.insert(4, "인천")
-        txt.insert(5, "인천")
-        txt.insert(6, "인천")
-        txt.insert(7, "인천")
-        txt.pack(side=LEFT, fill=BOTH)
-        scrollbar.config(command=txt.yview)
 
 def main():
-    root = Tk()
-    root.title("TourAPI")
-    root.geometry("320x80+100+100")
-    myframe = MyFrame(root)
-    root.mainloop()
+
+    url = http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=
+    pass
 
 if __name__ == '__main__':
     main()
