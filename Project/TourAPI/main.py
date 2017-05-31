9a081d9cfbe33798aaf5029c20f787f5fd96cc57
Kyoroo <elzmfptpseh@naver.com>
2017-05-31 19:33:19 +0900

    Add files via upload
    빼앵

--- Project/TourAPI/main.py
+++ Project/TourAPI/main.py
@@ -8,16 +8,37 @@ def main():
     url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=GY1KfpRH3x3fR4MpYAhLtGfpn%2BgzOAUXv86hfjkvhfZEi6BZSv2oEY%2BO28UjOyNhogZFh81Fv04pz5us%2FkIYkA%3D%3D&areaCode="+str(areaCode)+"&numOfRows=20&pageNo=1&MobileOS=ETC&MobileApp=AppTesting"
 
     url2 = urllib.request.urlopen(url).read()
-    url2 = url2.decode("UTF-8")
-    et = ET.fromstring(str(url2))
-    iter = et.getiterator("item")
+    #url2 = url2.decode("UTF-8")
+    #et = ET.fromstring(str(url2))
+    #iter = et.getiterator("item")
 
-    for i in iter:
-        code = i.find("code")
-        name = i.find("name")
-        list.append((code.text, name.text))
+    f = open("tour_01.xml", "wb")
+    f.write(url2)
+    f.close()
 
-    print(list)
+    tree = ET.parse("tour_01.xml")
+    root = tree.getroot()
+
+    x_points = []
+    y_points = []
+
+    for x_point in root.iter("code"):
+        x_points.append(x_point.text)
+    for y_point in root.iter("name"):
+        y_points.append(y_point.text)
+
+    for i in x_points:
+        print(i)
+    for y in y_points:
+        print(y)
+
+
+    #for i in iter:
+    #    code = i.find("code")
+    #    name = i.find("name")
+    #    list.append((code.text, name.text))
+
+    #print(list)
 
 if __name__ == '__main__':
     main()
