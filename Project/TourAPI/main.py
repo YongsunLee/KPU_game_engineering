26c8c2641ddc151dfe8c534eb9ee24aa6e28fd54
Kyoroo <elzmfptpseh@naver.com>
2017-05-31 20:58:39 +0900

    주소에서 좌표를 받아 맵을 띄우는 것으로 수정

--- Project/TourAPI/main.py
+++ Project/TourAPI/main.py
@@ -1,12 +1,15 @@
 # -*- coding : UTF-8 -*-
 import urllib.request
 import xml.etree.ElementTree as ET
+import Navermap
+import webbrowser
 
 def main():
     list = []
-    areaCode = 1
-    url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=GY1KfpRH3x3fR4MpYAhLtGfpn%2BgzOAUXv86hfjkvhfZEi6BZSv2oEY%2BO28UjOyNhogZFh81Fv04pz5us%2FkIYkA%3D%3D&areaCode="+str(areaCode)+"&numOfRows=20&pageNo=1&MobileOS=ETC&MobileApp=AppTesting"
+    areaCode = 35
+    #url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=GY1KfpRH3x3fR4MpYAhLtGfpn%2BgzOAUXv86hfjkvhfZEi6BZSv2oEY%2BO28UjOyNhogZFh81Fv04pz5us%2FkIYkA%3D%3D&areaCode=" + str(areaCode) + "&numOfRows=100&MobileOS=ETC&MobileApp=AppTesting"
 
+    url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?ServiceKey=GY1KfpRH3x3fR4MpYAhLtGfpn%2BgzOAUXv86hfjkvhfZEi6BZSv2oEY%2BO28UjOyNhogZFh81Fv04pz5us%2FkIYkA%3D%3D&areaCode="+ str(areaCode) +"&numOfRows=100&MobileOS=ETC&MobileApp=AppTesting"
     url2 = urllib.request.urlopen(url).read()
     #url2 = url2.decode("UTF-8")
     #et = ET.fromstring(str(url2))
@@ -22,16 +25,15 @@ def main():
     x_points = []
     y_points = []
 
-    for x_point in root.iter("code"):
+    for x_point in root.iter("mapx"):
         x_points.append(x_point.text)
-    for y_point in root.iter("name"):
+    for y_point in root.iter("mapy"):
         y_points.append(y_point.text)
 
-    for i in x_points:
-        print(i)
-    for y in y_points:
-        print(y)
-
+    url_3 = Navermap.Set_map(x_points[0], y_points[0])
+    print(x_points[0])
+    print(y_points[0])
+    webbrowser.open_new(url_3)
 
     #for i in iter:
     #    code = i.find("code")
