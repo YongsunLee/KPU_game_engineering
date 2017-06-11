b15b777ab2a84fd294520c016cdcdcd3a34b7228
Kyoroo <elzmfptpseh@naver.com>
2017-06-12 01:41:03 +0900

    Add files via upload
    조건부 검색 추가

--- Project/TourAPI/main.py
+++ Project/TourAPI/main.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 import Navermap
 import webbrowser
 
-def main():
+def temp_main():
     list = []
     areaCode = 35
     #url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=GY1KfpRH3x3fR4MpYAhLtGfpn%2BgzOAUXv86hfjkvhfZEi6BZSv2oEY%2BO28UjOyNhogZFh81Fv04pz5us%2FkIYkA%3D%3D&areaCode=" + str(areaCode) + "&numOfRows=100&MobileOS=ETC&MobileApp=AppTesting"
@@ -55,5 +55,47 @@ def main():
 
     #print(list)
 
-if __name__ == '__main__':
-    main()
+def search_local(area_code, sigungu_code ,service_code):
+    url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?ServiceKey=GY1KfpRH3x3fR4MpYAhLtGfpn%2BgzOAUXv86hfjkvhfZEi6BZSv2oEY%2BO28UjOyNhogZFh81Fv04pz5us%2FkIYkA%3D%3D&areaCode=" + str(
+        area_code) + "&sigunguCode=" + str(sigungu_code) + "&numOfRows=20000&arrange=C&MobileOS=ETC&MobileApp=AppTesting"
+    url2 = urllib.request.urlopen(url).read()
+    print(url)
+
+    f = open("tour_area.xml", "wb")
+    f.write(url2)
+    f.close()
+
+    tree = ET.parse("tour_area.xml")
+    root = tree.getroot()
+
+    x_points = []
+    y_points = []
+    nameList = []
+    addrList = []
+    contentid = []
+
+    num = 0
+    for addr in root.iter("addr1"):
+        addrList.append(addr.text)
+    for x_point in root.iter("mapx"):
+        x_points.append(x_point.text)
+    for y_point in root.iter("mapy"):
+        y_points.append(y_point.text)
+    for name in root.iter("title"):
+        nameList.append(name.text)
+    for content in root.iter("contenttypeid"):
+        contentid.append(content.text)
+
+    for i in addrList:
+        if(contentid[num] == str(service_code)):
+            print(x_points[num])
+            print(y_points[num])
+            print(nameList[num])
+            print(addrList[num])
+            print()
+        num += 1
+
+
+#if __name__ == '__main__':
+#    main()
+search_local(5, 1, 32)
